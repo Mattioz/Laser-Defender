fa137451933e2a84074b3ff06d3540228a8929e4
Mattioz <61207452+Mattioz@users.noreply.github.com>
2024-02-17 00:43:05 -0300

    AudioPlayer - Singleton Pattern

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioPlayer : MonoBehaviour
@@ -11,6 +12,33 @@ public class AudioPlayer : MonoBehaviour
     [Header("Damage")]
     [SerializeField] AudioClip damageClip;
     [SerializeField] [Range(0f, 1f)] float damageVolume = 1f;
+
+    public static AudioPlayer instance;
+    //public AudioPlayer GetInstance()
+    //{
+    //    return instance;
+    //}
+
+    void Awake() 
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        //int instanceCount = FindObjectsOfType(GetType()).Length;
+        //if(instanceCount > 1)
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 
     public void PlayShootingClip()
     {
